09865b5795518e6f29860427a74bae9966d67697
Blipi <gpascualg93@gmail.com>
2013-10-30 01:55:58 +0100

    htWaiter: Use the Include path

--- htWait.sh
+++ htWait.sh
@@ -6,6 +6,6 @@ while true; do
     change=$(inotifywait -r -e close_write,moved_to,create . 2> /dev/null);
     change=${change#./ * };
     if [[ $change =~ (^|\ )\.htaccess$ ]]; then
-        perl /root/x-manager/htParser.pl "$change" `pwd`
+        perl -I /root/x-manager/ /root/x-manager/htParser2.pl "$change" `pwd`
     fi;
 done
